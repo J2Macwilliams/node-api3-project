afb35c3424a66fcf770cfd651ec55479e09a340c
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-04 13:12:06 -0800

    Require userDb and postDb into server.js .

--- server.js
+++ server.js
@@ -2,25 +2,34 @@ const express = require('express');
 
 const server = express();
 
+const userDb = require('./users/userDb');
+const postDb = require('./posts/postDb');
+
+// endpoints---------------------------------------------------
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
-// middleware
+
+// middleware--------------------------------------------------
 server.use(logger);
 server.use(validateUserId());
 server.use(validateUser);
 server.use(validatePost);
-//custom middleware
 
+//custom middleware--------------------------------------------
 function logger(req, res, next) {
   console.log(`[${new Date().toISOString()}] ${req.method} to ${req.originalUrl} `
   );
   next();
 }
 
-function validateUserId() {
+function validateUserId(id) {
   return function (req, res, next) {
+if (id && id === req.params.id) {
 
+}else {
+  res.status(400).json({ message: "invalid user id" })
+}
   }
 }
 
