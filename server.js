662c7648e1de017a2d347992c404afdec3c41d8c
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-04 13:30:16 -0800

    Require userRoutes and postRoutes. Invoke respective routes with server.use. Add validateUserId() on userRoutes invoke.

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express');
+const userRoutes = require('./users/userRouter');
+const postRoutes = require('./posts/postRouter');
 
 const server = express();
 
 const userDb = require('./users/userDb');
-const postDb = require('./posts/postDb');
+// const postDb = require('./posts/postDb');
+
+
 
 // endpoints---------------------------------------------------
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use('/api/users', validateUserId(id), userRoutes);
+server.use('/api/posts', postRoutes);
+
 // middleware--------------------------------------------------
 server.use(logger);
-server.use(validateUserId());
 server.use(validateUser);
 server.use(validatePost);
 
