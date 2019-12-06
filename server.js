e9b01dedc850f0ac57538e189f1fac5bdf9e5807
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-05 16:28:15 -0800

    Add CORS as dependency. Require and invoke CORS.

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const userRoutes = require('./users/userRouter');
 const postRoutes = require('./posts/postRouter');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 // endpoints---------------------------------------------------
 server.get('/', (req, res) => {
